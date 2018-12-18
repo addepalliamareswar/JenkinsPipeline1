32038bbf562ed196f20f973a801768d6126ea491
Eswar <aaddepalli@vlocity.com>
2018-12-18 14:46:44 +0530

    new file:   JenkinsProject/.classpath 	new file:   JenkinsProject/.project 	new file:   JenkinsProject/.settings/org.eclipse.core.resources.prefs 	new file:   JenkinsProject/.settings/org.eclipse.jdt.core.prefs 	new file:   JenkinsProject/.settings/org.eclipse.m2e.core.prefs 	new file:   JenkinsProject/pom.xml 	new file:   JenkinsProject/src/test/java/SampleProject/JenkinsProject/HelloWorld.java

---
+++ JenkinsProject/src/test/java/SampleProject/JenkinsProject/HelloWorld.java
@@ -0,0 +1,14 @@
+package SampleProject.JenkinsProject;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+
+    @Test
+    public void test1 () {
+        
+        System.out.println("Welcome to Jenkins");
+    }
+    
+    
+}
